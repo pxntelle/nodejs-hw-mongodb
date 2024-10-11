fcb2bbc3ab99282ca39c74f15e8a3a8dd2fe27cb
pxntelle <taniapantelle@gmail.com>
2024-10-11 22:30:01 +0200

    find method fixed

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -42,7 +42,7 @@ export const getContacts = async ({
   };
 };
 
-export const getContact = (filter) => ContactCollection.findById(filter);
+export const getContact = (filter) => ContactCollection.findOne(filter);
 
 export const createContact = (payload) => ContactCollection.create(payload);
 
