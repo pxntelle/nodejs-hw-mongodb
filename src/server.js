36498f0b6c3ed621de51fe00f591368c28d2aa70
pxntelle <taniapantelle@gmail.com>
2024-10-14 01:11:13 +0200

    photo as file upload check on render

--- src/server.js
+++ src/server.js
@@ -35,8 +35,6 @@ export const setupServer = () => {
 
   app.use('*', notFoundHandler);
 
-  app.use(errorHandler);
-
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
